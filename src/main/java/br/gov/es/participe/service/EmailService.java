c8088f4fa3ea634ded25ba283e3728c164e99684
Giwberto Gill Pereira Leite <giwberto.leite@totemti.com.br>
2020-07-10 14:00:50 -0400

    PAR Sprint 4 Correções 2.1 OS2

--- src/main/java/br/gov/es/participe/service/EmailService.java
+++ src/main/java/br/gov/es/participe/service/EmailService.java
@@ -7,6 +7,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMailMessage;
@@ -23,6 +24,9 @@ public class EmailService {
 
 	@Autowired 
 	private JavaMailSender mailSender;
+	
+	@Value("${spring.mail.username}")
+    private String from;
 	
 	@Autowired
     private Configuration freemarkerConfig;
@@ -49,6 +53,7 @@ public class EmailService {
 		
 		try {
 			body = generateBody(data);
+			helper.setFrom(from);
 			helper.setTo(to);
 			helper.setText(body, true);
 	        helper.setSubject(title);
