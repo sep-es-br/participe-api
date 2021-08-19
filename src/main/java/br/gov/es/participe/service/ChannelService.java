ffe362edbaad26d6744a821db317460d1eccc310
Igor Marques de Oliveira <igor.oliveira@totemti.com.br>
2021-08-19 10:22:22 -0400

    Fixing meeting.

--- src/main/java/br/gov/es/participe/service/ChannelService.java
+++ src/main/java/br/gov/es/participe/service/ChannelService.java
@@ -1,53 +1,61 @@
 package br.gov.es.participe.service;
 
-import java.util.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.gov.es.participe.controller.dto.ChannelDto;
 import br.gov.es.participe.model.Channel;
 import br.gov.es.participe.model.Meeting;
 import br.gov.es.participe.repository.ChannelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class ChannelService {
 
-    @Autowired
-    private ChannelRepository channelRepository;
+  private final ChannelRepository channelRepository;
 
-    public Set<Channel> saveChannelsMeeting(List<ChannelDto> channelsDto, Meeting meeting) {
-        Set<Channel> channelsSaved = meeting.getChannels();
-        channelsSaved.removeIf(
-                channelSaved -> channelsDto.stream().noneMatch(filtro -> filtro.getId().equals(channelSaved.getId())));
+  @Autowired
+  public ChannelService(ChannelRepository channelRepository) {
+    this.channelRepository = channelRepository;
+  }
 
-        channelsDto.forEach(channel -> {
-            if (channel.getId() == null) {
-                Channel channelEntity = new Channel(channel.getName(), channel.getUrl());
-                channelsSaved.add(channelEntity);
-            }
+  public Set<Channel> saveChannelsMeeting(List<ChannelDto> channelsDto, Meeting meeting) {
+    Set<Channel> storedChannels = meeting.getChannels();
+    storedChannels.removeIf(
+      channelSaved -> channelsDto.stream()
+        .filter(dto -> dto.getId() != null)
+        .noneMatch(channel -> channel.getId().equals(channelSaved.getId())));
 
-            Optional<Channel> channelFinded = channelsSaved.stream().filter(filtro -> {
-                if(filtro.getId() == null) return false;
+    channelsDto.forEach(channel -> {
+      if(channel.getId() == null) {
+        Channel channelEntity = new Channel(channel.getName(), channel.getUrl());
+        storedChannels.add(channelEntity);
+      }
 
-                return filtro.getId().equals(channel.getId());
-            }).findFirst();
+      Optional<Channel> channelFound = storedChannels.stream().filter(filtro -> {
+        if(filtro.getId() == null) return false;
 
-            if (!channelFinded.isPresent())
-                return;
+        return filtro.getId().equals(channel.getId());
+      }).findFirst();
 
-            Channel channelUpdated = channelFinded.get();
-            channelUpdated.setName(channel.getName());
-            channelUpdated.setUrl(channel.getUrl());
-        });
+      if(!channelFound.isPresent()) {
+        return;
+      }
 
-        this.channelRepository.saveAll(channelsSaved);
+      Channel channelUpdated = channelFound.get();
+      channelUpdated.setName(channel.getName());
+      channelUpdated.setUrl(channel.getUrl());
+    });
 
-        return channelsSaved;
-    }
+    this.channelRepository.saveAll(storedChannels);
 
-    public Channel save(Channel channel) {
-        return channelRepository.save(channel);
-    }
+    return storedChannels;
+  }
+
+  public Channel save(Channel channel) {
+    return channelRepository.save(channel);
+  }
 
 }
