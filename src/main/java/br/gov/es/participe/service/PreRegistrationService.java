84dd6c49307438cb9601b512d55a7bbe4f110f72
Bruno Matos <brunomatosbarbosa@hotmail.com>
2024-03-08 17:03:24 -0300

    Salvar data de check in no pre-registration

--- src/main/java/br/gov/es/participe/service/PreRegistrationService.java
+++ src/main/java/br/gov/es/participe/service/PreRegistrationService.java
@@ -2,6 +2,13 @@ package br.gov.es.participe.service;
 
 import br.gov.es.participe.model.*;
 import br.gov.es.participe.repository.*;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.transaction.annotation.Transactional;
@@ -45,5 +52,15 @@ public class PreRegistrationService {
 
     }
 
-    
+    public void saveCheckIn(Long personId, Long meetingId){
+
+        PreRegistration preRegistrationStored =  preRegistrationRepository.findByMeetingAndPerson(meetingId, personId);
+
+        if(preRegistrationStored != null){
+            preRegistrationStored.setCheckIn(new Date());
+            preRegistrationRepository.save(preRegistrationStored);
+        }
+
+    }
+
 }
