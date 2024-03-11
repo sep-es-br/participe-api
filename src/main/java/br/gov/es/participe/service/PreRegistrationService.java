e658a7b9ec601dce1942311a06a304d047e369f1
Bruno Matos <brunomatosbarbosa@hotmail.com>
2024-03-11 16:03:18 -0300

    add: enviar email pre registro

--- src/main/java/br/gov/es/participe/service/PreRegistrationService.java
+++ src/main/java/br/gov/es/participe/service/PreRegistrationService.java
@@ -3,7 +3,18 @@ package br.gov.es.participe.service;
 import br.gov.es.participe.model.*;
 import br.gov.es.participe.repository.*;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.text.DateFormatter;
 
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
@@ -59,4 +70,36 @@ public class PreRegistrationService {
 
     }
 
+
+    public Map<String, String> buildEmailBody(Meeting meeting, byte[] imageQR){
+        String MICROREGIAO = "microregiao";
+        String LOCAL = "localDaReuniao";
+        String ENDERECO = "enderecoDaReuniao";
+        String MUNICIPIO = "municipio";
+        String NOMEAUDIENCIA = "nomeDaAudiencia";
+        String DATA = "dataDaReuniao";
+        String QRCODE = "qrcode";
+
+        Map<String, String> data = new HashMap<>();
+
+        data.put(MICROREGIAO, meeting.getName());
+        data.put(LOCAL, meeting.getPlace());
+        data.put(ENDERECO, meeting.getAddress());
+        data.put(MUNICIPIO, meeting.getLocalityPlace().getName());
+        data.put(NOMEAUDIENCIA, meeting.getConference().getName());
+        data.put(QRCODE, Base64.getEncoder().encodeToString(imageQR));
+        
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss");
+        Instant beginDateInstant = meeting.getBeginDate().toInstant();
+        Instant endDateInstant = meeting.getEndDate().toInstant();
+        LocalDateTime beginDate = LocalDateTime.ofInstant(beginDateInstant, ZoneId.systemDefault());
+        LocalDateTime endDate = LocalDateTime.ofInstant(endDateInstant, ZoneId.systemDefault());
+        
+        data.put(DATA, beginDate.format(formatter)+" a "+endDate.format(formatter));
+
+
+        return data;
+
+    }
 }
