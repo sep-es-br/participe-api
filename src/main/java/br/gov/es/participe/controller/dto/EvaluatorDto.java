de2eb778cbba05816efca0908ba4b3fc1a85dcf5
Artur Faria <artur.faria.1992@gmail.com>
2024-05-13 17:35:43 -0300

    feat: cria dto EvaluatorDto

---
+++ src/main/java/br/gov/es/participe/controller/dto/EvaluatorDto.java
@@ -0,0 +1,27 @@
+package br.gov.es.participe.controller.dto;
+
+import br.gov.es.participe.model.Evaluator;
+
+public class EvaluatorDto extends EvaluatorParamDto {
+    private Long id;
+
+    public EvaluatorDto() {
+    
+    }
+
+    public EvaluatorDto(Evaluator evaluator) {
+        this.id = evaluator.getId();
+        this.setOrganizationGuid(evaluator.getOrganization());
+        this.setSectionsGuid(evaluator.getSections());
+        this.setServersGuid(evaluator.getServers());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
