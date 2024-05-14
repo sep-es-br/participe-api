8e73c2fccc0290b42615a660bd6bcafc9f474929
Artur Faria <artur.faria.1992@gmail.com>
2024-05-14 14:43:32 -0300

    ref: remove comentarios, imports nao utilizados

--- src/main/java/br/gov/es/participe/controller/EvaluationSectionsController.java
+++ src/main/java/br/gov/es/participe/controller/EvaluationSectionsController.java
@@ -18,18 +18,12 @@ import br.gov.es.participe.service.EvaluatorService;
 import br.gov.es.participe.service.PersonService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-
-
-
 
 @RestController
 @CrossOrigin
@@ -70,11 +64,6 @@ public class EvaluationSectionsController {
         }
 
         Evaluator newEvaluator = evaluatorService.saveEvaluator(evaluatorParamDto);
-
-        // if(newEvaluator == null) {
-        //     // return ResponseEntity.status(400).body(null);
-        //     return ResponseEntity.status(400).body("Já existe uma entidade avaliadora com o guid desta organização.");
-        // }
 
         EvaluatorDto response = new EvaluatorDto(newEvaluator);
 
