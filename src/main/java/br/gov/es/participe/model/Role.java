3f198352bae8903fbf15224986b0fbc0c40cf1a3
Artur Faria <artur.faria.1992@gmail.com>
2024-05-22 16:58:31 -0300

    ref: adiciona relacionamento BELONGS_TO de Role para Section

--- src/main/java/br/gov/es/participe/model/Role.java
+++ src/main/java/br/gov/es/participe/model/Role.java
@@ -1,12 +1,23 @@
 package br.gov.es.participe.model;
 
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
 
 @NodeEntity
 public class Role extends Evaluator {
-    
+
+    @Relationship(value = "BELONGS_TO", direction = Relationship.OUTGOING)
+    private Section section;
+
     public Role(String guid) {
         this.setGuid(guid);
     }
+    
+    public Section getSection() {
+        return section;
+    }
 
+    public void setSection(Section section) {
+        this.section = section;
+    }
 }
