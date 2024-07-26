4c08f88aab77f30d09556120c28782d88e1341bc
Bruno Matos <brunomatosbarbosa@hotmail.com>
2024-07-26 09:31:43 -0300

    fix: removendo import

--- src/main/java/br/gov/es/participe/service/ProposalEvaluationService.java
+++ src/main/java/br/gov/es/participe/service/ProposalEvaluationService.java
@@ -9,8 +9,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import javax.swing.text.html.Option;
-
 import java.util.Base64;
 
 import org.apache.tomcat.util.json.ParseException;
