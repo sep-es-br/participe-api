fe8753513bfa8f6e0b65ba54eb212c43819308da
geanCarneiro <geancarneiro1@gmail.com>
2025-08-05 17:09:02 -0300

    logging

--- src/main/java/br/gov/es/participe/controller/ResourceExceptionHandler.java
+++ src/main/java/br/gov/es/participe/controller/ResourceExceptionHandler.java
@@ -9,7 +9,6 @@ import br.gov.es.participe.exception.EvaluatorForbiddenException;
 import br.gov.es.participe.exception.NotFoundException;
 import br.gov.es.participe.util.dto.MessageDto;
 
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,29 +17,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.http.MediaType;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
   @Autowired
-  private Logger log;
+  private Logger log = Logger.getGlobal();
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<MessageDto> handleException(Exception e, HttpServletRequest req) {
-    log.error("Error ", e);
+    log.log(Level.SEVERE, "Error ", e);
     return ResponseEntity.status(500).body(new MessageDto(500, e.getMessage()));
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<MessageDto> handleException(IllegalArgumentException e, HttpServletRequest req) {
-    log.error("Bad Request", e);
+    log.log(Level.SEVERE, "Bad Request", e);
     return ResponseEntity.status(400).body(new MessageDto(400, e.getMessage()));
   }
 
   @ExceptionHandler(IllegalStateException.class)
   public ResponseEntity<MessageDto> handleException(IllegalStateException e, HttpServletRequest req) {
-    log.error("Bad Request", e);
+    log.log(Level.SEVERE, "Bad Request", e);
     return ResponseEntity.badRequest().body(new MessageDto(HttpStatus.BAD_REQUEST.value(),
                                                            e.getMessage()
     ));
@@ -48,51 +49,51 @@ public class ResourceExceptionHandler {
 
   @ExceptionHandler(IOException.class)
   public ResponseEntity<MessageDto> handleException(IOException e, HttpServletRequest req) {
-    log.error("Error", e);
+    log.log(Level.SEVERE, "Error", e);
     return ResponseEntity.status(500).contentType(MediaType.APPLICATION_JSON).body(
       new MessageDto(500, e.getMessage() + " -> " + e.getCause()));
   }
 
   @ExceptionHandler(RuntimeException.class)
   public ResponseEntity<MessageDto> handleException(RuntimeException e, HttpServletRequest req) {
-    log.error("Error", e);
+    log.log(Level.SEVERE, "Error", e);
     return ResponseEntity.status(500).body(new MessageDto(500, e.getMessage()));
   }
 
 
   @ExceptionHandler(QRCodeGenerateException.class)
   public ResponseEntity<MessageDto> handleException(QRCodeGenerateException e, HttpServletRequest req) {
-    log.error("Error", e);
+    log.log(Level.SEVERE, "Error", e);
     return ResponseEntity.status(400).body(new MessageDto(400, e.getMessage()));
   }
 
   @ExceptionHandler(ParticipeServiceException.class)
   public ResponseEntity<MessageDto> participeServiceHandler(ParticipeServiceException e) {
-    log.error("Error", e);
+    log.log(Level.SEVERE, "Error", e);
     return ResponseEntity.status(400).body(new MessageDto(400, e.getMessage()));
   }
 
   @ExceptionHandler(ApiAcessoCidadaoException.class)
   public ResponseEntity<MessageDto> handleException(ApiAcessoCidadaoException e) {
-    log.error("Error", e);
+    log.log(Level.SEVERE, "Error", e);
     return ResponseEntity.status(500).body(new MessageDto(500, e.getMessage()));
   }
 
   @ExceptionHandler(ApiOrganogramaException.class)
   public ResponseEntity<MessageDto> handleException(ApiOrganogramaException e) {
-    log.error("Error", e);
+    log.log(Level.SEVERE, "Error", e);
     return ResponseEntity.status(500).body(new MessageDto(500, e.getMessage()));
   }
 
   @ExceptionHandler(EvaluatorForbiddenException.class)
   public ResponseEntity<MessageDto> handleException(EvaluatorForbiddenException e) {
-    log.error("Error", e);
+    log.log(Level.SEVERE, "Error", e);
     return ResponseEntity.status(403).body(new MessageDto(403, e.getMessage()));
   }
 
   @ExceptionHandler(NotFoundException.class)
   public ResponseEntity<MessageDto> handleException(NotFoundException e){
-    log.error("Error", e);
+    log.log(Level.SEVERE, "Error", e);
     return ResponseEntity.status(404).body(new MessageDto(404, e.getMessage()));
   }
 }
