e8725468e8d596fdd630f37aff5cee11a9a11a0a
geanCarneiro <geancarneiro1@gmail.com>
2026-05-27 16:29:28 -0300

    .

--- src/main/java/br/gov/es/participe/repository/PersonRepository.java
+++ src/main/java/br/gov/es/participe/repository/PersonRepository.java
@@ -56,8 +56,9 @@ public interface PersonRepository extends Neo4jRepository<Person, Long> {
   )
   Optional<Person> findByLoginEmail(@Param("email")String email);
 
-    @Query("MATCH (person:Person)-[authBy:IS_AUTHENTICATED_BY]-(:AuthService)\n" +
+    @Query("MATCH (person:Person)-[authBy:IS_AUTHENTICATED_BY]->(authService:AuthService)\n" +
     "    WHERE authBy.idByAuth=$sub\n" +
+    "    OPTIONAL MATCH (l:Login)<-[:MADE]-(person)\n" +
     "    RETURN person\n" +
     "    ORDER BY l.time IS NOT NULL ASC, l.time DESC\n" +
     "    LIMIT 1"
