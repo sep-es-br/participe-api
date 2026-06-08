eab11e7de95fd2a459722cf56470c9ef25ded7b3
geanCarneiro <geancarneiro1@gmail.com>
2026-06-08 16:50:18 -0300

    add validação de campos

--- src/main/java/br/gov/es/participe/controller/dto/AuthorityCredentialRequest.java
+++ src/main/java/br/gov/es/participe/controller/dto/AuthorityCredentialRequest.java
@@ -4,20 +4,48 @@
  */
 package br.gov.es.participe.controller.dto;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 /**
  *
  * @author desenvolvimento
  */
 public class AuthorityCredentialRequest {
+    
+    @Positive(message = "O campo deve ser positivo")
+    @NotBlank(message = "O campo não pode ser vazio")
     private Long madeBy;
+    
+     
     private String representedByCpf;
+    
+    @NotBlank(message = "O campo não pode ser vazio")
     private String representedByEmail;
+    
+    @NotBlank(message = "O campo não pode ser vazio")
     private String representedByName;
+    
+    @NotBlank(message = "O campo não pode ser vazio")
     private String representedBySub;
+    
+    @Positive(message = "O campo deve ser positivo")
+    @NotBlank(message = "O campo não pode ser vazio")
     private Long meetingId;
+    
+    @NotNull
+    @Valid
     private OptionOrganization organization;
+    
+    @NotBlank
     private String role;
+    
+    @Positive
+    @NotBlank
     private Long localityId;
+    
     private Boolean isTeam;
 
     public String getRepresentedBySub() {
