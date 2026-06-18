a783c45805fb6695d492ad9d5b05e8e5eae1ac4d
geanCarneiro <geancarneiro1@gmail.com>
2026-06-18 12:43:01 -0300

    fix do bug absurdo

--- src/main/java/br/gov/es/participe/service/PersonService.java
+++ src/main/java/br/gov/es/participe/service/PersonService.java
@@ -327,7 +327,7 @@ public class PersonService {
     this.verifyMakeLoginCondition(person, makeLogin, authService, conferenceId);
 
     if (persistRelationship) {
-      isAuthenticatedByRepository.save(authenticatedBy, 0);
+      isAuthenticatedByRepository.save(authenticatedBy);
       log.info("Alterações no relacionamento isAuthenticatedById={} persistidas com sucesso", authenticatedBy.getId());
     }
     return person;
