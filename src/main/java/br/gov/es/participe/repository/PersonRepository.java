a52296725fb0f56ef6aa13e19d6d3b69adf10067
geanCarneiro <geancarneiro1@gmail.com>
2026-06-26 14:24:50 -0300

    .

--- src/main/java/br/gov/es/participe/repository/PersonRepository.java
+++ src/main/java/br/gov/es/participe/repository/PersonRepository.java
@@ -333,7 +333,7 @@ public interface PersonRepository extends Neo4jRepository<Person, Long> {
             "  coalesce(coalesce(cia, pr).isAuthority, false) AS isAuthority,\n" +
             "  coalesce(coalesce(cia, pr).isTeam, false) AS isTeam,\n" +
             "  coalesce(coalesce(cia, pr).role, '') AS role,\n" +
-            "  coalesce(coalesce(cia, pr).organization, '') + ' ' + coalesce(coalesce(cia, pr).organizationShort, '') AS organization,\n" +
+            "  coalesce(coalesce(cia, pr).organizationShort, '') + ' - ' + coalesce(coalesce(cia, pr).organization, '') AS organization,\n" +
             "  coalesce(coalesce(cia, pr).isAnnounced, false) AS isAnnounced,  \n" +
             "  coalesce(coalesce(cia, pr).toAnnounce, false) AS toAnnounce,\n" +
             "  pr.created AS preRegisteredDate,\n" +
