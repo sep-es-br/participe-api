c2f7372c34edfe5fe642e2423f7300b6ac86dff4
geanCarneiro <geancarneiro1@gmail.com>
2026-07-08 10:25:55 -0300

    .

--- src/main/java/br/gov/es/participe/repository/PersonRepository.java
+++ src/main/java/br/gov/es/participe/repository/PersonRepository.java
@@ -246,7 +246,7 @@ public interface PersonRepository extends Neo4jRepository<Person, Long> {
     "    COALESCE(ci.isAnnounced, false) AS announced, \n" +
     "    p.name AS name, \n" +
     "    ci.role AS role,  \n" +
-    "    ci.organization as organization,\n" +
+    "    ci.organizationShort + ' - ' + ci.organization as organization,\n" +
     "    ci\n" +
     "ORDER BY \n" +
     "    apoc.coll.max([\n" +
